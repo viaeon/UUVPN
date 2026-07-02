948fcee8faf98705ba768532216277d56e2fb11f
admin <admin@viaeon.com>
2026-07-02 19:05:23 +0800

    Fix: Improve bridging header with conditional include

--- iOS-SwiftUI-Code/Library/Library-Bridging-Header.h
+++ iOS-SwiftUI-Code/Library/Library-Bridging-Header.h
@@ -5,4 +5,11 @@
 //  Bridging header for Libbox.xcframework
 //
 
+// Import Libbox Objective-C headers for Swift access
+// Note: The framework must be linked and framework search paths must be set correctly
+
+#if __has_include(<Libbox/Libbox.h>)
 #import <Libbox/Libbox.h>
+#elif __has_include("Libbox.h")
+#import "Libbox.h"
+#endif
